b5c9a1cc5246c361b4ec2f4d304868e655b6c879
safeedha <mksafeedha@gmail.com>
2025-07-04 09:43:26 +0530

    cors error changed

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -12,7 +12,7 @@ const server = createServer(appInstances.app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://medimate-rust.vercel.app'],
     credentials: true
   }
 });
--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,4 +1,8 @@
 import { io } from "socket.io-client";
-export const socket = io('http://localhost:3000', {
+
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+export const socket = io(BACKEND_URL, {
   withCredentials: true,
-  });
+});
